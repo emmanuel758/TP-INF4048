143de476873eb511995e9fd8b853d28e983cd7ee
Codeur16 <charlesnjiosseu@gmail.com>
2024-06-28 07:01:39 +0100

    2

--- exo4/exo4.c
+++ exo4/exo4.c
@@ -1,4 +1,4 @@
-/*
+/* Enonce
  * Écrire un analyseur qui permet de renvoyer les lexèmes d’une chaine arithmétique
  * permettant de faire les additions et les multiplications.Exemple : L’analyse 21 + 12 * 45 devra renvoyer:
  */
@@ -26,7 +26,7 @@ void afficher(int valeur, Etat etatCourant)
 
     // Vérifie si l'état courant est un opérateur
     if (etatCourant == etatOperateur)
-        printf("<oper:%c>", valeur); // Affiche les opérateurs au format <oper:caractère>
+        printf("<oper:%c>", valeur); // Affiche les opérateurs au format <oper:caractereactère>
 }
 
 // Fonction principale d'analyse lexicale
@@ -34,27 +34,21 @@ void afficher(int valeur, Etat etatCourant)
  * Cette fonction prend un pointeur vers un fichier en lecture et
  * retourne un entier qui représente le succès ou l'échec de l'analyse lexicale.
  */
-int analyseLexical(FILE *fdw)
+int analyseLexical(FILE *file)
 {
-    int nbOc = 0, i = 0, j = 0, state = -1, nbr = 0, nbr1; // Variables non utilisées
-    int init = 0;                                          // Variable non utilisée
+    int  nbr = 0;
+    int init = 0;                                          
     Etat q = etatInitial, q_precedent = etatInitial;       // Déclare et initialise les états
-    char car, tmp;                                         // Déclare les variables pour lire les caractères
+    char caractere;                                         // Déclare les variables pour lire les caractereactères
 
-    /*
-     * nbr:Pour accumuler les chiffres et former un nombre.
-     * q : Pour suivre l 'état actuel de l' analyse.
-     * car : Pour stocker le caractère lu du fichier.
-     * char car, tmp;
-     */
 
     printf("\n"); // Aller a la ligne
 
-    while (!feof(fdw)) // La boucle continue tant que la fin du fichier (feof(fdw)) n'est pas atteinte
+    while (!feof(file)) // La boucle continue tant que la fin du fichier (feof(file)) n'est pas atteinte
     {
-        fread(&car, 1, 1, fdw); // fread(&car, 1, 1, fdw) lit un caractère du fichier et le stocke dans car.
+        fread(&caractere, 1, 1, file); // fread(&caractere, 1, 1, file) lit un caractereactère du fichier et le stocke dans caractere.
 
-        switch (car)
+        switch (caractere)
         {
         case '0':
         case '1':
@@ -67,7 +61,7 @@ int analyseLexical(FILE *fdw)
         case '8':
         case '9':
             q = etatNombre;
-            nbr = nbr * 10 + (car - '0');
+            nbr = nbr * 10 + (caractere - '0');
             break;
 
         case '+':
@@ -81,17 +75,17 @@ int analyseLexical(FILE *fdw)
             }
 
             q = etatOperateur;
-            afficher(car, q);
+            afficher(caractere, q);
             break;
         case '\n':
             break;
 
         default:
-            printf("\n%c : Parse Error, unkown char\n", car);
+            printf("\n%c : Parse Error, unkown char\n", caractere);
             return 1;
         }
     }
-    fclose(fdw);
+    fclose(file);
     if (q == etatNombre)
         afficher(nbr, q); // Affiche le dernier nombre si nécessaire
     printf("\n\n");
@@ -101,8 +95,8 @@ int analyseLexical(FILE *fdw)
 // Fonction principale du programme
 int main(int argc, char **argv)
 {
-    FILE *fdw = fopen(argv[1], "r");   // Ouvre le fichier passé en argument
-    int valAnal = analyseLexical(fdw); // Appelle la fonction d'analyse lexicale
+    FILE *file = fopen(argv[1], "r");   // Ouvre le fichier passé en argument
+    int valAnal = analyseLexical(file); // Appelle la fonction d'analyse lexicale
 
     return 0; // Termine le programme
 }
